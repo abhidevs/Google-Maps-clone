feb96b4baac7b8aa5c3cad8e1e79533200fa055a
Abhik Das <abhikdas98765@gmail.com>
2021-04-03 20:49:57 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,33 @@
+mapboxgl.accessToken =
+  "pk.eyJ1IjoiYWJoaWsxMDEiLCJhIjoiY2toaHZjZjE0MGtzOTJ4cDZpOXc4OTl5ZSJ9.mPfIgPcmDOMIvp_j5JfLuQ";
+
+navigator.geolocation.getCurrentPosition(successLocation, errorLocation, {
+  enableHighAccuracy: true,
+});
+
+function successLocation(position) {
+  console.log(position);
+  setupMap([position.coords.longitude, position.coords.latitude]);
+}
+
+function errorLocation() {
+  setupMap([-0.127758, 51.507351]);
+}
+
+function setupMap(center) {
+  const map = new mapboxgl.Map({
+    container: "map",
+    style: "mapbox://styles/mapbox/streets-v11",
+    center: center,
+    zoom: 15,
+  });
+
+  const nav = new mapboxgl.NavigationControl();
+  map.addControl(nav);
+
+  const directions = new MapboxDirections({
+    accessToken: mapboxgl.accessToken,
+  });
+  
+  map.addControl(directions, 'top-left');
+}
